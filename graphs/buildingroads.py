5ed989502e5602cba7ade80134b7569613ef6c46
Rahul Mahajan <rahulmahajan98645@gmail.com>
2021-05-18 03:59:09 +0530

    added

---
+++ graphs/buildingroads.py
@@ -0,0 +1,54 @@
+n,m=map(int,input().split())
+
+vis=[]
+arr=[]
+
+for _ in range(n):
+    arr.append([])
+    vis.append([])
+
+for _ in range(n):
+    # for i in range(m):
+    u=input()
+    arr[_].append(u.split())
+
+
+
+for _ in range(n):
+    for i in range(m):
+        if(arr[_][0][i]=='#'):
+            vis[_].append(1)
+        else:
+            vis[_].append(0)
+
+for _ in range(len(vis)):
+    print(vis[_])
+
+
+
+ans=0
+
+moves=[[-1,0],[1,0],[0,-1],[0,1]]
+
+def valid(x,y):
+    if(x<0 or x>=n or y<0 or y>=m):
+        return False
+    if(vis[x][y]==1):
+        return False
+    return True
+
+def dfs(i,j):
+    vis[i][j]=1
+    for _ in range(len(moves)):
+        if(valid(i+moves[_][0],j+moves[_][1])):
+            dfs(i+moves[_][0],j+moves[_][1])
+
+def cnctd():
+    for _ in range(n):
+        for i in range(m):
+            if(vis[_][i]==0):
+                dfs(_,i)
+                ans+=1
+
+
+print(ans)
